482aefd80463c7bcc3c88a9e6e7ab4f8c10712ca
estefanomisme <3401@holbertonschool.com>
2022-04-21 22:20:09 -0500

    Lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Preparation for technical job interview"""
+
+
+class Box:
+    """Defines a class for a box in order to know if the box
+    is opened or not"""
+    def __init__(self, keys, opened):
+        self.keys = keys
+        self.opened = opened
+
+
+def setOpened(listOfBoxes, currentBox):
+    """Open the boxes of the list depending of the given keys"""
+    for key in currentBox.keys:
+        if listOfBoxes[key].opened is False:
+            listOfBoxes[key].opened = True
+            listOfBoxes = setOpened(listOfBoxes, listOfBoxes[key])
+    return listOfBoxes
+
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    for i in range(len(boxes)):
+        if i == 0:
+            boxes[i] = Box(boxes[i], True)
+        else:
+            boxes[i] = Box(boxes[i], False)
+    boxes = setOpened(boxes, boxes[0])
+    for box in boxes:
+        if box.opened is False:
+            return False
+    return True
